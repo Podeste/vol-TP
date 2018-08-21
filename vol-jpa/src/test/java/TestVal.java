2623027bf5ad7457df24b555ce412f4dd5e1c222
Unknown <valentin.peclard@hotmail.fr>
2018-08-21 14:52:54 +0200

    jessaipa

---
+++ vol-jpa/src/test/java/TestVal.java
@@ -0,0 +1,14 @@
+import sopra.promo404.vol.Singleton;
+import sopra.promo404.vol.dao.IDaoReservation;
+import sopra.promo404.vol.model.Reservation;
+
+public class TestVal {
+
+	public static void main(String[] args) {
+		
+		IDaoReservation daoReservation = Singleton.getInstance().getDaoReservation();
+		
+		Reservation resa001 = new Reservation("")
+	}
+
+}
